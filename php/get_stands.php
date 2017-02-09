7a1a43523e92174d761a20785fff414df7deca71
ascencioMari <ascencio_mari@yahoo.com.mx>
2017-02-09 00:17:03 -0600

    T

--- php/get_stands.php
+++ php/get_stands.php
@@ -4,11 +4,7 @@ $host="localhost";
 $dbname="acoeoco_stands2017";
 $user="acoeoco_ad_stand";
 $pass="AdminE2016";
-<<<<<<< HEAD
 
-=======
-
->>>>>>> parent of 68f1f19... Usuario y  password nuevos
 $connection = mysql_connect($host,$user,$pass);
 
 $db = mysql_select_db($dbname, $connection);
