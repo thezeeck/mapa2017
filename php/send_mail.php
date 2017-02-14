cc6fdda0bceff160b569a8f2166428bdb6292232
ascencioMari <ascencio_mari@yahoo.com.mx>
2017-02-14 00:18:42 -0600

    envio de correo

---
+++ php/send_mail.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['email'])) {
+
+$email_to = $_POST['email'];
+$email_subject = "Solicitud de reserva";
+
+// Validar los datos ingresados por el usuario
+if(!isset($_POST['solicitud']) ||
+!isset($_POST['empresa']) ||
+!isset($_POST['nombre']) ||
+!isset($_POST['telefono'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Solicitud: " . $_POST['solicitud'] . "\n";
+$email_message .= "Empresa: " . $_POST['empresa'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "Teléfono: " . $_POST['telephone'] . "\n\n";
+
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡La solicitud ha sido enviado!";
+}
+?>
